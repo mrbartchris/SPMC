1d60510fd7440e4fca0e0f7526b14ea7ed2c9647
Christian Bartolo <Chris@Christians-MacBook-Pro.local>
2016-11-28 11:52:56 +0100

    Third commit: - New header class (including initilization of functions) - main.c completed

---
+++ SPMCalc/functions.h
@@ -0,0 +1,26 @@
+//
+// Created by Christian Bartolo on 28/11/2016.
+//
+
+#ifndef SPMCALC_FUNCTIONS_H
+#define SPMCALC_FUNCTIONS_H
+
+#endif //SPMCALC_FUNCTIONS_H
+
+
+
+float add(){
+
+}
+
+float sub(){
+
+}
+
+float mul(){
+
+}
+
+float div(){
+
+}
--- SPMCalc/main.c
+++ SPMCalc/main.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include "functions.h"
 
 int main(){
+    float res = 0;
+
+    char choice;
+
+    while(choice != 'q'){
+        printf("\n\n******** CALCULATOR ********");
+        printf("\na: ADD          b: SUBTRACT");
+        printf("\nc: MULTIPLY     d: DIVIDE");
+        printf("\nEnter \'q\' to quit.");
+        printf("\n\nEnter your choice: ");
+        scanf("%c", &choice);
+        getchar();
+
+        switch (choice) {
+            case 'a':
+                res = add();
+                break;
+            case 'b':
+                res = sub();
+                break;
+            case 'c':
+                res = mul();
+                break;
+            case'd':
+                res = div();
+                break;
+            case 'q':
+                printf("Terminating...");
+                continue;
+            default:
+                printf("\nIncorrect input\n");
+                continue;
+        }
+
+        printf("\nResult: %.2f",res);
+
+    }
 
     return 0;
 }
