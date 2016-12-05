f9c2b644b90ca97fb5c6a7597476d33ab7337afb
Malcolm Abdilla <malcolmabdilla@gmail.com>
2016-12-05 12:54:43 +0100

    Added Subtraction and division functions

--- SPMCalc/functions.h
+++ SPMCalc/functions.h
@@ -30,10 +30,20 @@ float sub(){
     return ans;
 }
 
-float mul(){
-
+float mul(float n1,float n2){
+    printf("Enter a number");
+    scanf("%f",&n1);
+    printf("Enter another number to be subtracted from");
+    scanf("%f",&n2);
+    float s = n1-n2;
+    return s;
 }
 
-float div(){
-
+float div(float n1,float n2){
+    printf("Enter a number");
+    scanf("%f",&n1);
+    printf("Enter another number to be divided from");
+    scanf("%f",&n2);
+    float s = n1/n2;
+    return s;
 }
