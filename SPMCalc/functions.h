142772f5153bf901fc5e90be1eb8bd75dda7c244
Gabriel Farrugia <gfarrug@gmail.com>
2016-12-05 12:48:12 +0100

    Addition and Subtraction functions added

--- SPMCalc/functions.h
+++ SPMCalc/functions.h
@@ -2,19 +2,32 @@
 // Created by Christian Bartolo on 28/11/2016.
 //
 
+#include <stdio.h>
+
 #ifndef SPMCALC_FUNCTIONS_H
 #define SPMCALC_FUNCTIONS_H
 
 #endif //SPMCALC_FUNCTIONS_H
 
-
+float x, y, ans;
 
 float add(){
+printf("Enter two numbers to be added:\n");
+    scanf("%f",&x);
+    scanf("%f",&y);
+    ans = x+y;
 
+    return ans;
 }
 
 float sub(){
+    printf("Enter a number:\n");
+    scanf("%f",&x);
+    printf("Enter number to be subtracted:\n");
+    scanf("%f",&y);
+    ans = x+y;
 
+    return ans;
 }
 
 float mul(){
